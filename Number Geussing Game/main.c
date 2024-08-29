6b5a9f61e17d12a59fb2ab53fe70367c53594a52
0x778 <hussamalmaswari@gmail.com>
2024-08-29 22:49:05 +0300

    Feat:Geusing Game in C

--- Number Geussing Game/main.c
+++ Number Geussing Game/main.c
@@ -1,19 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 
 
 
 void main(){
-int guessnumber , playernumber;
+srand(time(0));
 
-guessnumber= rand();
-printf('%s',"Enter Number from 0 - 10 :");
+int guessnumber , playernumber , max , min;
+max = 10;
+min = 0;
+guessnumber= rand() % (max - min + 1) + min;
+printf("%s","Enter Number from 0 - 10 :");
 scanf("%i",&playernumber);
 if(playernumber == guessnumber){
     printf("%s","great guess u win!!!");
 }
 else{
-printf("%s","sorry lucky anthor time");
+printf("%s , %i , %s","sorry lucky anthor time the number was :" ,guessnumber ,"\n");
 }
 }
