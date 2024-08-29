70c68bd411b1ef64c4f1e8c8c6cae7ae7001b5d6
0x778 <hussamalmaswari@gmail.com>
2024-08-29 19:47:00 +0300

    Feat:make the calculater Function

--- Simple Calculater/main.c
+++ Simple Calculater/main.c
@@ -22,24 +22,28 @@ int divide(int num1 , int num2){
 }
 
 void main(){
+calculater();
+}
+
+void calculater(){
 int num1 ; 
 printf("%s","write your first number!");
 scanf("%d",&num1);
- char symbol ; 
+char symbol ; 
 printf("%s","write your operation !");
 scanf(" %c",&symbol);
- int num2 ;
-  printf("%s","write your second number! ");
-  scanf("%d",&num2);
+int num2 ;
+printf("%s","write your second number! ");
+scanf("%d",&num2);
 
 if(symbol == '+')
     printf("%i",sum(num1,num2));
 if (symbol == '-')
     printf("%i", minus(num1,num2));
 if (symbol == '*')
-    multi(num1,num2);
+   printf("%i",multi(num1,num2));
 if (symbol == '/')
-    div(num1,num2);
+   printf("%i",div(num1,num2));
 if (symbol == '%')
-    mod(num1,num2);
+   printf("%i",mod(num1,num2));
 }
