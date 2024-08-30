2bf9958e79f6d143c4c970629fde4850cda5595a
0x778 <hussamalmaswari@gmail.com>
2024-08-30 21:40:00 +0300

    feat:new way to reverse the string

--- Stuff in C/String /TODO/main.c
+++ Stuff in C/String /TODO/main.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
-//Where is the Error
+//Reverse the string using pointers or array
 void RevString(char * Word){
 int len = strlen(Word);
 int locate = len -1;
@@ -20,8 +20,28 @@ printf("%s \n" , temp) ;
 free(temp);
 }
 
-void main(){
+//Reverse the string using Recursion 
+void ReverseRecursion(char * string , int start , int end){
+    if(start >= end){
+        return ;
+    }
+    char temp = string[start];
+    string[start] = string[end];
+    string[end] = temp ; 
+    ReverseRecursion(string , start +1 , end -1 );
+}
+ //best practice
+int main(){
 // char * Revstrign = RevString("Hi");
-char * string = "Hiiiiiiiiiii";
-RevString(string);
+// char * string = "Hiiiiiiiiiii";
+// RevString(string);
+char amount[100] = "I am hussam i speak arabic only";
+ReverseRecursion(amount , 0 , strlen(amount) - 1);
+printf("%s  \n",amount);
+
+
+//using build in function supported by some compilers like Turbo C
+// printf("%s \n",strrev(amount));
+
+return 0 ;
 }
